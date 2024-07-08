1d4c1a7d1845c3e489ced9bd026515bb75324e6e
Nolan Le <nhatle.stu@gmail.com>
2024-07-07 22:42:30 -0600

    run file

--- 2023/main.py
+++ 2023/main.py
@@ -1,6 +1,10 @@
-from src.day3 import Part2
+from src.day1 import Solution
+from src.day2 import Solution
+from src.day3 import Part1, Part2
+from src.day4 import Solution
+from src.day5 import Solution 
 
 if __name__ == "__main__":
     file_name = input("Enter file name: ")
-    ob = Part2(file_name=file_name)
-    print(ob.sum_part_numbers())
+    ob = Solution()
+    print(ob.solve_part1(file_name=file_name))

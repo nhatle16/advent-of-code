9cca32a85a1a74099f2fcf8995b8a31c7861a37c
Nolan Le <nhatle.stu@gmail.com>
2024-07-07 22:39:27 -0600

    source code of day 4 and day 5

---
+++ 2023/src/day4.py
@@ -0,0 +1,83 @@
+# DAY 4 - ADVENT OF CODE
+# https://adventofcode.com/2023/day/4
+
+import os
+
+class Solution():
+    def __init__(self, file_name):
+        self.__cards = None
+        
+        current_dir = os.path.dirname(__file__)
+        parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
+        input_file = os.path.join(parent_dir, 'input', file_name)
+        
+        with open (file=input_file, mode='r') as fin:
+            # Get the information of the scratchcards
+            # regardless of card numbers
+            self.__cards = [list(line.strip()[line.find(':')+1:].split()) for line in fin]
+    
+    # PART ONE
+    def calculate_points(self):
+        points = 0
+        
+        for card in self.__cards:
+            bar = card.index('|')
+            
+            win_numbers = card[:bar]
+            own_numbers = card[bar+1:]
+            
+            i = 0     # Create 2 pointers to traverse 2 lists
+            count = 0
+            first = False   # Create a flag
+            
+            while i < len(win_numbers):
+                if win_numbers[i] in own_numbers:
+                    if not first:
+                        count = 1
+                        first = True    # Set flag into True
+                    else: 
+                        count *= 2  # Each match after that makes the point doubled
+                i += 1
+            points += count
+            
+        return points
+    
+    # PART TWO
+    def count_scratchcards(self):
+        card_dict = dict()
+        ord = 1
+        
+        for card in self.__cards:
+            bar = card.index('|')
+            
+            win_numbers = card[:bar]
+            own_numbers = card[bar+1:]
+            
+            count = 0   
+            
+            for i in range(len(win_numbers)):
+                if win_numbers[i] in own_numbers:
+                    count += 1
+                    
+            card_dict[ord] = [count, 1]     # First element is matches, second element is number of copy
+            ord += 1
+        
+        # Go through each key in the dictionary
+        for k in range(1, len(card_dict)+1):
+            matches, copies = card_dict[k]  
+            # Access the subordinates of current card
+            # subordinates are cards to be added copies that lie
+            # within the number of matches of current card
+            for i in range(1, matches+1):
+                if k + i in card_dict:
+                    card_dict[k+i][1] += copies     # Add current card number of copies to the copies of its subordinates
+                                                    
+        
+        total = sum(copies for (matches, copies) in card_dict.values())
+        return total
+    
+    def solve_part1(self):
+        return self.calculate_points()
+    
+    def solve_part2(self):
+        return self.count_scratchcards()
---
+++ 2023/src/day5.py
@@ -0,0 +1,59 @@
+# DAY 5 - ADVENT OF CODE
+# https://adventofcode.com/2023/day/5
+
+from typing import List, Tuple
+import os
+
+class Solution():
+    def calculate_dest(self, src: int, start_des: int, start_src: int, range: int) -> Tuple[int, bool]:
+        '''
+        Find the corresponding destination spot of the source, has a flag to show succesful conversion
+        '''        
+        if start_src + range <= src or src < start_src:
+            return src, False
+        return src - (start_src - start_des), True
+    
+    def get_seed(self, line: str) -> List[int]:
+        '''
+        Retrieve the list of seed numbers to be planted
+        '''        
+        return [int(x) for x in line.split()[1:]]
+    
+    def solve_part1(self, file_name) -> List[int]:
+        # Retrieve the path to input file from another package
+        current_dir = os.path.dirname(__file__)
+        parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
+        input_file = os.path.join(parent_dir, 'input', file_name)
+        
+        with open(file=input_file, mode='r') as fin:
+            # First line contains the seed numbers
+            seeds = self.get_seed(fin.readline().strip())
+            while True:
+                current_line = fin.readline()
+
+                # Check if it reads the end of file
+                if not current_line:
+                    break
+                
+                # Remove the endline character
+                current_line = current_line.strip()
+        
+                if current_line.endswith("map:"):
+                    maps = []
+                    
+                    # Dive into the maps and retrieve information
+                    while True:
+                        current_line = fin.readline().strip()
+                        if not current_line:
+                            break
+                        numbers = [int(x) for x in current_line.split()]
+                        maps.append(numbers)
+                    
+                    for i in range(len(seeds)):
+                        for line in maps:
+                            dest, flag = self.calculate_dest(src=seeds[i], start_des=line[0], start_src=line[1], range=line[2])
+                            if flag:
+                                seeds[i] = dest
+                                break            
+                        
+        return min(seeds)

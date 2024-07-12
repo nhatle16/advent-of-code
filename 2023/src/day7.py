60f794b06ff91dfed55012353abc11cac75ce9f8
Nolan Le <nhatle.stu@gmail.com>
2024-07-12 17:24:20 -0600

    update source code with part 2

--- 2023/src/day7.py
+++ 2023/src/day7.py
@@ -8,6 +8,7 @@ from functools import cmp_to_key
 import os
 
 class Solution():
+    # PART ONE
     def get_strength(self, label: str) -> int:
         return int(label) if label.isdigit() else {'A': 14, 'K': 13, 'Q': 12, 'J': 11, 'T': 10}.get(label, 0)
     
@@ -34,16 +35,15 @@ class Solution():
         elif l == 2:
             return "full" if 3 in card_count.values() else "four"
         else:
-            return "five"          
+            return "five"     
     
     def compare_hands(self, pair1: Tuple[str, int], pair2: Tuple[str, int]) -> int:
         hand1, hand2 = pair1[0], pair2[0]
         for i in range(len(hand1)):
             if self.get_strength(hand1[i]) < self.get_strength(hand2[i]):
-                return -1
-            elif self.get_strength(hand1[i]) > self.get_strength(hand2[i]):
                 return 1
-            i += 1
+            elif self.get_strength(hand1[i]) > self.get_strength(hand2[i]):
+                return -1
         return 0            
     
     def find_total_winnings(self, pairs: List[Tuple[str, int]]) -> dict:
@@ -59,13 +59,82 @@ class Solution():
         output = 0
         
         for type in sorted(ctgs.keys(), key=self.type_strength, reverse=True):
-            hands_sorted = sorted(ctgs[type], key=cmp_to_key(self.compare_hands), reverse=True)  # sort hands in increasing order
+            hands_sorted = sorted(ctgs[type], key=cmp_to_key(self.compare_hands))  # sort hands in increasing order
             for i, (hand, bid) in enumerate(hands_sorted):
                 output += bid * rank
                 rank -= 1
                 
         return output
-                
-    def solve_quiz(self, file_name):
+    
+    def solve_part1(self, file_name):
         pairs = self.get_hands_bids(file_name=file_name)
         return self.find_total_winnings(pairs)
+    
+    # PART TWO
+    def get_strength2(self, label: str) -> int:
+        return int(label) if label.isdigit() else {'A': 13, 'K': 12, 'Q': 11, 'T': 10, 'J': 1}.get(label, 0)
+    
+    def categorize_hand2(self, hand: str) -> str:
+        if 'J' not in hand:
+            return self.categorize_hand(hand)
+        
+        j_freq = hand.count('J')
+        labels = sorted(Counter(hand).values(), reverse=True)
+        
+        if j_freq == 5:
+            return "five"
+        if j_freq == 4:
+            return "five"
+        elif j_freq == 3:
+            if labels == [3,2]:
+                return "five"
+            return "four"
+        elif j_freq == 2:
+            if labels == [3,2]:
+                return "five"
+            elif labels == [2,2,1]:
+                return "four"
+            return "three"
+        else:
+            if labels == [4,1]:
+                return "five"
+            elif labels == [3,1,1]:
+                return "four"
+            elif labels == [2,2,1]:
+                return "full"
+            elif labels == [2,1,1,1]:
+                return "three"
+            return "one"
+
+    def compare_hands2(self, pair1: Tuple[str, int], pair2: Tuple[str, int]) -> int:
+        hand1, hand2 = pair1[0], pair2[0]
+        for i in range(len(hand1)):
+            if self.get_strength2(hand1[i]) < self.get_strength2(hand2[i]):
+                return 1
+            elif self.get_strength2(hand1[i]) > self.get_strength2(hand2[i]):
+                return -1
+        return 0
+        
+    def find_total_winnings2(self, pairs: List[Tuple[str, int]]) -> dict:
+        ctgs = {}
+        
+        for hand, bid in pairs:
+            type = self.categorize_hand2(hand)
+            if type not in ctgs:
+                ctgs[type] = list()
+            ctgs[type].append((hand, bid))
+            
+        rank = len(pairs)  
+        output = 0
+        
+        for type in sorted(ctgs.keys(), key=self.type_strength, reverse=True):
+            hands_sorted = sorted(ctgs[type], key=cmp_to_key(self.compare_hands2))  # sort hands in increasing order
+            for i, (hand, bid) in enumerate(hands_sorted):
+                output += bid * rank
+                rank -= 1
+                
+        return output
+
+    def solve_part2(self, file_name):
+        pairs = self.get_hands_bids(file_name=file_name)
+        return self.find_total_winnings2(pairs)
